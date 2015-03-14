69d27a181f71869798063a462b7d0ed69f8ad448
Kumar Gaurav <kgthegreat@gmail.com>
2015-03-14 19:20:42 +0530

    basic classification of politics and tech tweets complete. all tests passing

--- sensible.go
+++ sensible.go
@@ -1,38 +1,55 @@
 package main
+
 import (
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"html/template"
+	"log"
 	"net/http"
 	"net/url"
+	_ "regexp"
 	"strings"
-	_ "html/template"
-	"github.com/ChimeraCoder/anaconda"
 )
+
 const (
-	consumerKey = "6CfGgaWC42SlfSHJ0AusSH64j"
-	consumerSecret = "WGGQwcaDZ6mMyHB9WoZgTyJFczHzy865GPiCL5EeukFoPanmvH"
-	accessToken = "14741206-HBQKBY4WHW93EWVv1bgLhBlLrrRqMwgdTpEoDreiI" 
+	consumerKey       = "6CfGgaWC42SlfSHJ0AusSH64j"
+	consumerSecret    = "WGGQwcaDZ6mMyHB9WoZgTyJFczHzy865GPiCL5EeukFoPanmvH"
+	accessToken       = "14741206-HBQKBY4WHW93EWVv1bgLhBlLrrRqMwgdTpEoDreiI"
 	accessTokenSecret = "NgvGtxH5UaGiX3hbcXKbjuB5LbVS4WXWyg960XC4w0Ksn"
 )
 
-	var api = anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+var api = anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+//var templates = template.Must(template.ParseFiles("index.html"))
+var timelineTweets []anaconda.Tweet
+
+type Page struct {
+	Title  string
+	Tweets []anaconda.Tweet
+	Length int
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	v := url.Values{}
-	v.Set("count", "20")
-	timelineTweets, err := api.GetHomeTimeline(v)
-	if err != nil {
-		fmt.Fprintln(w, err)
-	}
 	techTweets := getTechTweets(timelineTweets)
-	_ = techTweets
-	
+//	classifiedTweets := classifyTweets(timelineTweets)
+	p := &Page{Title: "Tech Tweets", Tweets: techTweets}
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, p)
+}
+
+func politicsHandler(w http.ResponseWriter, r *http.Request) {
+
+	tweets := getPoliticsTweets(timelineTweets)
+	p := &Page{Title: "Politics Tweets", Tweets: tweets, Length: len(tweets)}
+	t, _ := template.ParseFiles("politics.html")
+	t.Execute(w, p)
 }
 
 func getTechTweets(timelineTweets []anaconda.Tweet) []anaconda.Tweet {
-	techKeywords := []string{"android", "golang"}
+	techKeywords := []string{"golang", "ruby", "devs", "developers", "android", "ios", "programming", "code", "java", "coders", "developer", "fullstack", "full stack", "product", "hack", "hacker", "bug", "technology", "software"}
 	var techTweets []anaconda.Tweet
-	for _ , tweet := range timelineTweets {
-		for _ , kwd := range techKeywords {
+	for _, tweet := range timelineTweets {
+		for _, kwd := range techKeywords {
+			//			match, _ := regexp.MatchString("."+kwd+".", tweet.Text)
 			if strings.Contains(tweet.Text, kwd) {
 				techTweets = append(techTweets, tweet)
 				break
@@ -42,53 +59,67 @@ func getTechTweets(timelineTweets []anaconda.Tweet) []anaconda.Tweet {
 	return techTweets
 }
 
- 
-func publishSearchResults(api *anaconda.TwitterApi, w http.ResponseWriter, v url.Values) {
-	searchResult, err := api.GetSearch("gophercon", v)
-	if err != nil {
-		fmt.Fprintln(w, err)
+func classifyTweets(timelineTweets []anaconda.Tweet) map[string][]anaconda.Tweet {
+	classifiedTweets := make(map[string][]anaconda.Tweet)
+	var techTweets []anaconda.Tweet
+	var politicsTweets []anaconda.Tweet
+	for _, tweet := range timelineTweets {
+		if itIs("tech", tweet) {
+			techTweets = append(techTweets, tweet)
+		}
+		if itIs("politics", tweet) {
+			politicsTweets = append(politicsTweets, tweet)
+		}
 	}
-	for _ , tweet := range searchResult.Statuses {
-		fmt.Fprintln(w, tweet.Text)
-	}
-
+	classifiedTweets["tech"] = techTweets
+	classifiedTweets["politics"] = politicsTweets
+	return classifiedTweets
 }
 
-func getTimeline(api *anaconda.TwitterApi, w http.ResponseWriter, v url.Values) {
-	timeline, err := api.GetHomeTimeline(v)
-	techKeywords := []string{"golang", "ruby", "devs", "developers", "android", "ios", "app", "programming", "code", "java"}
-	var techTweets []anaconda.Tweet
-	var nonTechTweets []anaconda.Tweet
-	if err != nil {
-		fmt.Fprintln(w, err)
+func itIs(context string, tweet anaconda.Tweet) bool {
+	keywordMap := make(map[string][]string)
+	keywordMap["tech"] = []string{"golang", "ruby", "devs", "developers", "android", "ios", "programming", "code", "java", "coders", "developer", "fullstack", "full stack", "product", "hack", "hacker", "bug", "technology", "software"}
+	keywordMap["politics"] = []string{"modi", "congress", "bjp", "rahul gandhi", "manmohan singh", "narendra modi"}
+	for _, keyword := range keywordMap[context] {
+		if strings.Contains(tweet.Text, keyword) {
+			return true
+		}
 	}
-	for _ , tweet := range timeline {
-		for _ , kwd := range techKeywords {
+	return false
+}
+
+
+func getPoliticsTweets(timelineTweets []anaconda.Tweet) []anaconda.Tweet {
+	techKeywords := []string{"bjp", "modi", "congress", "sonia gandhi", "rahul baba", "arnab", "budget"}
+	var techTweets []anaconda.Tweet
+	for _, tweet := range timelineTweets {
+		for _, kwd := range techKeywords {
+			//			match, _ := regexp.MatchString("."+kwd+".", tweet.Text)
 			if strings.Contains(tweet.Text, kwd) {
 				techTweets = append(techTweets, tweet)
 				break
-			} else {
-				nonTechTweets = append(nonTechTweets, tweet)
-				break
 			}
 		}
-		
 	}
-	fmt.Fprintln(w, "-------------------Tech Tweets--------------------")
-	for _ , t := range techTweets {
-		fmt.Fprintln(w, t.Text)
-	}
-	fmt.Fprintln(w, "-------------------NonTech Tweets--------------------")
-	for _ , nt := range nonTechTweets {
-		fmt.Fprintln(w, nt.Text)
-	}
+	return techTweets
+}
 
+func getTimelineTweets() []anaconda.Tweet{
+	v := url.Values{}
+	v.Set("count", "200")
+	timelineTweets, err := api.GetHomeTimeline(v)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return timelineTweets
 }
 
 func main() {
- 	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerKey(consumerKey)
 	anaconda.SetConsumerSecret(consumerSecret)
-
+	timelineTweets = getTimelineTweets()
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/politics", politicsHandler)
+//	http.HandleFunc("/sports", sportsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- sensible_test.go
+++ sensible_test.go
@@ -3,67 +3,149 @@ package main
 import (
 	"testing"
 	"github.com/ChimeraCoder/anaconda"
+	_ "fmt"
 )
+
+var politicsTweet = anaconda.Tweet {
+		Text: "Narendra modi going to Sri Lanka",
+	}
+
+var techTweet1 = anaconda.Tweet {
+		Text: "Looking for android developer",
+	}
+var techTweet2 = anaconda.Tweet {
+		Text: "The newest thing in golang",
+	}
+
+var nonTechTweet = anaconda.Tweet {
+		Text: "This is not related",
+	}
+var techTweetMixed = anaconda.Tweet {
+		Text: "Looking for android developer golang",
+	}
+
+var nonTechTweet2 = anaconda.Tweet {
+		Text: "The newest thing in happy",
+	}
 
 func TestCanary(t *testing.T) {
 }
 func TestTechTweetsShouldIncludeAndroidTweets(t *testing.T) {
-	techTweet1 := anaconda.Tweet {
-		Text: "Looking for android developer",
-	}
-	nonTechTweet := anaconda.Tweet {
-		Text: "This is not related",
-	}
 	expected := []anaconda.Tweet{techTweet1}
 	timelineTweets := []anaconda.Tweet{techTweet1, nonTechTweet}
 	actual := getTechTweets(timelineTweets)
 	if len(actual) != len(expected) {
-		t.Errorf("Wanted: %v, Got: %v", expected, actual)
+		t.Errorf("Did not match")
 	}
-
-
 }
 	
 func TestTechTweetsShouldIncludeAllRelevantTweets(t *testing.T) {
-	techTweet1 := anaconda.Tweet {
-		Text: "Looking for android developer",
-	}
-	techTweet2 := anaconda.Tweet {
-		Text: "The newest thing in golang",
-	}
-
-	nonTechTweet := anaconda.Tweet {
-		Text: "This is not related",
-	}
 	timelineTweets := []anaconda.Tweet{techTweet1, techTweet2, nonTechTweet}
 	expected := []anaconda.Tweet{techTweet1, techTweet2}
-
 	actual := getTechTweets(timelineTweets)
 	if len(actual) != len(expected) {
-		t.Errorf("Wanted: %v, Got: %v", expected, actual)
+		t.Errorf("Did not match")
 	}
-
-
 }
 
 func TestTechTweetsShouldIncludeARelevantTweetOnlyOnce(t *testing.T) {
-	techTweet1 := anaconda.Tweet {
-		Text: "Looking for android developer golang",
+	timelineTweets := []anaconda.Tweet{techTweetMixed, techTweet2, nonTechTweet}
+	expected := []anaconda.Tweet{techTweet1, techTweet2}
+	actual := getTechTweets(timelineTweets)
+	if len(actual) != len(expected) {
+		t.Errorf("Did not match")
 	}
-	techTweet2 := anaconda.Tweet {
-		Text: "The newest thing in golang",
+}
+
+func TestTechTweetsShouldIncludeOnlyWithActualKeywords(t *testing.T) {
+	timelineTweets := []anaconda.Tweet{techTweetMixed, nonTechTweet2, nonTechTweet}
+	expected := []anaconda.Tweet{techTweet1}
+	actual := getTechTweets(timelineTweets)
+	if len(actual) != len(expected) {
+		t.Errorf("Did not match")
+	}
+}
+
+func TestCanClassifyTwoDifferentTweets(t *testing.T) {
+	timelineTweets := []anaconda.Tweet{techTweet1, politicsTweet}
+	expected := map[string][]anaconda.Tweet{
+		"tech": []anaconda.Tweet{techTweet1},
+		"politics": []anaconda.Tweet{politicsTweet},
 	}
 
-	nonTechTweet := anaconda.Tweet {
-		Text: "This is not related",
+	actual := classifyTweets(timelineTweets)
+ 	if len(actual["tech"]) != len(expected["tech"]) {
+		t.Errorf("Tech did not match. Actual length : %v, Expected length : %v",len(actual["tech"]),len(expected["tech"]))
 	}
-	timelineTweets := []anaconda.Tweet{techTweet1, techTweet2, nonTechTweet}
-	expected := []anaconda.Tweet{techTweet1, techTweet2}
+ 	if len(actual["politics"]) != len(expected["politics"]) {
+		t.Errorf("Politics did not match. Actual length : %v, Expected length : %v",len(actual["politics"]),len(expected["politics"]))
+	}
+
+	if actual["tech"] != nil && actual["tech"][0].Text != expected["tech"][0].Text {
+		t.Errorf("Did not match. Actual tweet : %v, Expected tweet : %v",actual["tech"][0].Text,expected["tech"][0].Text)
+	}
+	if actual["politics"] != nil && actual["politics"][0].Text != expected["politics"][0].Text {
+		t.Errorf("Did not match. Actual tweet : %v, Expected tweet : %v",actual["politics"][0].Text,expected["politics"][0].Text)
+	}
+}
+
+func TestCanClassifyOneTechTweet(t *testing.T) {
+	timelineTweets := []anaconda.Tweet{techTweet1}
+	expected := map[string][]anaconda.Tweet{
+		"tech": []anaconda.Tweet{techTweet1},
+	}
+
+	actual := classifyTweets(timelineTweets)
+	if len(actual["tech"]) != len(expected["tech"]) {
+		t.Errorf("Did not match. Actual length : %v, Expected length : %v",len(actual["tech"]),len(expected["tech"]))
+	}
+	if actual["tech"] != nil && actual["tech"][0].Text != expected["tech"][0].Text {
+		t.Errorf("Did not match. Actual tweet : %v, Expected tweet : %v",actual["tech"][0].Text,expected["tech"][0].Text)
+	}
+}
+
+
+func TestATechTweetIsIdentifiedAsTech(t *testing.T) {
+	if !itIs("tech", techTweet1) {
+		t.Errorf("Did not classify")
+	}
+}
+
+func TestAnotherTechTweetIsIdentifiedAsTech(t *testing.T) {
+	if !itIs("tech", techTweet2) {
+		t.Errorf("Did not classify")
+	}
+}
+
+func TestContextBasedIdentificationOfTweet(t *testing.T) {
+	if !itIs("tech", techTweet1) {
+		t.Errorf("Did not classify")
+	}
+	
+	if !itIs("politics", politicsTweet) {
+		t.Errorf("Could not classify politics tweet")
+	}
+}
+/*
+func TestCanClassifyOneTweet(t *testing.T) {
+	techTweet := anaconda.Tweet {
+		Text: "Looking for android developer golang",
+	}
+	politicsTweet := anaconda.Tweet {
+		Text: "The latest thing in India is BJP government",
+	}
+	
+	timelineTweets := []anaconda.Tweet{techTweet, politicsTweet}
+	expected := map[string][]anaconda.Tweet{
+		"tech": []anaconda.Tweet{techTweet1},
+		"politics": []anaconda.Tweet{politicsTweet},
+	}
 
 	actual := getTechTweets(timelineTweets)
 	if len(actual) != len(expected) {
-		t.Errorf("Wanted: %v, Got: %v", expected, actual)
+		t.Errorf("Did not match")
 	}
 
 
-}
+	
+}*/
