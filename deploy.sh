c9cfc9fb6764bac6576d843efdc501f5fdb179d9
Kumar Gaurav <kumar.gaurav@skyscanner.net>
2019-07-03 08:58:27 +0100

    removed superfluous div

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ rsync -av templates kgthegreat@46.101.51.200:/home/kgthegreat/sensible
 echo "templates copied to server ..."
 rsync -av token.json kgthegreat@46.101.51.200:/home/kgthegreat/sensible
 echo "Token copied to server ..."
-rsync -av keyword_template.json kgthegreat@46.101.51.200:/home/kgthegreat/sensible
-echo "Keyword template copied to server ..."
-rsync -av keyword.json kgthegreat@46.101.51.200:/home/kgthegreat/sensible
-echo "Keyword seed copied to server ..."
+#rsync -av keyword_template.json kgthegreat@46.101.51.200:/home/kgthegreat/sensible
+#echo "Keyword template copied to server ..."
+#rsync -av keyword.json kgthegreat@46.101.51.200:/home/kgthegreat/sensible
+#echo "Keyword seed copied to server ..."
 echo "Deployment complete."
